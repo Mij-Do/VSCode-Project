dfa35c76f4abfb9c59e1017b6da926b6e6baca9f
ahmed <ahmedmijdo2@gmail.com>
2025-09-01 08:45:44 +0300

    refactor render file icons

--- src/components/RenderFileIcon.tsx
+++ src/components/RenderFileIcon.tsx
@@ -1,5 +1,6 @@
 import IconImg from "./IconImg";
 import FileIcon from "./SVG/File";
+import {extensionIconPath} from "../constant/index";
 
 interface IProps {
     filename: string;
@@ -10,28 +11,14 @@ interface IProps {
 const RenderFileIcon = ({filename, isFolder, isOpen}: IProps) => {
     const extention = filename.split('.').pop();
 
-    // files
-    if (extention === "tsx") return <IconImg src="/icons/react_ts.svg"/>
-    if (extention === "jsx") return <IconImg src="/icons/react.svg"/>
-    if (extention === "ts") return <IconImg src="/icons/typescript.svg"/>
-    if (extention === "js") return <IconImg src="/icons/javascript.svg"/>
-    if (extention === "css") return <IconImg src="/icons/css.svg"/>
-    if (extention === "html") return <IconImg src="/icons/html.svg"/>
+    if(extention && Object.prototype.hasOwnProperty.call(extensionIconPath, extention)) {
+        const IconPath = isFolder ? isOpen ? 
+        `${extensionIconPath[extention]}-open.svg` :
+        `${extensionIconPath[extention]}.svg` :
+        `${extensionIconPath[extention]}.svg`
 
-    // folders
-    if (extention === "node_modules" && isFolder) return isOpen ? 
-    <IconImg src="/icons/folder-node-open.svg"/> : <IconImg src="/icons/folder-node.svg"/>
-    if (extention === "public" && isFolder) return isOpen ? 
-    <IconImg src="/icons/folder-public-open.svg"/> : <IconImg src="/icons/folder-public.svg"/>
-    if (extention === "svg" && isFolder) return isOpen ? 
-    <IconImg src="/icons/folder-svg-open.svg"/> : <IconImg src="/icons/folder-svg.svg"/>
-    if (extention === "src" && isFolder) return isOpen ? 
-    <IconImg src="/icons/folder-src-open.svg"/> : <IconImg src="/icons/folder-src.svg"/>
-    if (extention === "components" && isFolder) return isOpen ? 
-    <IconImg src="/icons/folder-components-open.svg"/> : <IconImg src="/icons/folder-components.svg"/>
-
-
-
+        return <IconImg src={IconPath}/>
+    }
 
 
     if(isFolder && isOpen) return <IconImg src="/icons/folder-default-open.svg"/>
